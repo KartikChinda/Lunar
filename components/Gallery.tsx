a980eb3d2ede5529720170f0c68200cdabc690d7
Kartik Chinda <kartik.chinda@gmail.com>
2024-06-11 19:10:38 +0530

    Added links

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,20 +1,35 @@
-
+"use client";
 import Image from 'next/image'
 import React from 'react';
 
 // image imports
 import images from '@/assets';
-
+import "./gallery.css"
+import Link from 'next/link';
 
 const Gallery = () => {
+
+    const arr = [...Array(100)];
+
     return (
-        <section>
-            <div id='preview-img'>
-                <Image src={images[0]} alt='Watch1' />
-
+        <section className='w-[100vh] h-[90vh]'>
+            <div id='preview'>
+                <Image src={images[9]} alt='alt' />
             </div>
+            <div id="galleryContainer">
+                <div id="gallery">
+                    <div>
+                        {arr.map((_, index) => {
+                            return (
+                                <Link href={`/collections/${index % 10}`}>
+                                    <Image key={index} id='item' className='item' src={images[index % 10]} alt='alt' />
+                                </Link>
+                            )
+                        })}
+                    </div>
 
-
+                </div>
+            </div>
         </section>
 
     )
