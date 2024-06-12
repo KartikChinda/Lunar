e287730d28f8e46105acf835d741acb72463ff64
Kartik Chinda <kartik.chinda@gmail.com>
2024-06-12 20:02:21 +0530

    Added layout for the watchPage

--- app/collections/[id]/page.tsx
+++ app/collections/[id]/page.tsx
@@ -9,17 +9,29 @@ const page = () => {
     const params = useParams();
     const watchID = params.id;
 
+    const watchNumber = Number(watchID) < 9 ? `0${Number(watchID) + 1}` : Number(watchID) + 1;
+
     console.log(watchID)
 
     return (
         <section >
-            <div className='h-[86.5vh] group'>
-
+            <div className='h-[86.5vh] group flex'>
+                {/* animation code here  */}
                 <div className='absolute w-[51vw]  h-[86.8vh] transition-all duration-[1800ms] left-0 group-hover:left-[-52%] flex justify-center delay-300 items-center bg-palette-text text-palette-bg'>
                     <p className='relative left-24 lg:left-72 top-14  text-5xl font-text'>S A E A</p>
                 </div>
                 <div className='absolute w-[51vw]  h-[86.8vh] transition-all duration-[1800ms] left-[50%] group-hover:left-[100%] flex justify-center items-center delay-300 bg-palette-bg text-palette-text'>
-                    <p className=' font-headings text-[350px] -skew-x-6 absolute left-20'>{`0${Number(watchID) + 1}`}</p>
+                    <p className=' font-headings text-[350px] -skew-x-6 absolute left-20'>{watchNumber}</p>
+                </div>
+
+                {/* Here is the code for the underlying website */}
+                <div className='w-[50vw] flex justify-center items-center'>
+                    {/* <WatchInformation /> */}
+                    watch info
+                </div>
+                <div className='w-[50vw] flex justify-center items-center'>
+                    watch image
+                    {/* <WatchImage /> */}
                 </div>
             </div>
             <div>
---
+++ components/WatchImage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const WatchImage = (watchID: string) => {
+    console.log(watchID);
+    return (
+        <div>WatchImage</div>
+    )
+}
+
+export default WatchImage
