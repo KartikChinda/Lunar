483afd41d5f1905d5f8cdbf6eaf120939a6aa3b5
Kartik Chinda <kartik.chinda@gmail.com>
2024-08-17 17:52:58 +0530

    Added the UI for the products list and some UI changes to the footer.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,19 +6,19 @@ import { TiSocialInstagram } from "react-icons/ti";
 
 const Footer = () => {
     return (
-        <footer className="group bg-palette-text text-palette-bg border-t-2 pt-4 w-full">
+        <footer className="group bg-palette-bg text-palette-text border-t-2 pt-4 w-full">
 
             {/* Social Media Links */}
             <div className='flex gap-8 justify-center items-center text-4xl group duration-150'>
-                <Link href={"https://github.com/KartikChinda/musicMonthly"} className='hover:text-5xl duration-150'> <FaGithubSquare /></Link>
+                <Link href={"https://github.com/KartikChinda/WeSellItAll"} className='hover:text-5xl duration-150'> <FaGithubSquare /></Link>
                 <Link href={"https://twitter.com/ChindaKartik"} className='hover:text-5xl duration-150'> <FaSquareXTwitter /></Link>
                 <Link href={"https://www.linkedin.com/in/kartikchinda"} className='hover:text-5xl duration-150'> <FaLinkedin /></Link>
                 <Link href={"https://www.instagram.com/kartik.chinda11"} className='hover:text-5xl duration-150'> <TiSocialInstagram /></Link>
             </div>
             <div className="flex justify-center items-center">
                 {/* Large "Houseplant" Text */}
-                <h1 className=" group-hover:text-palette-pink font-headings text-white text-6xl md:text-8xl font-bold mt-4 duration-150">
-                    WE SELL IT ALL
+                <h1 className=" group-hover:text-palette-pink font-headings text-white text-6xl md:text-[150px] font-bold mt-4 duration-150 lg:tracking-widest">
+                    WE SELL IT ALL <span className='text-xl tracking-tighter -ml-4'>TM</span>
                 </h1>
             </div>
         </footer>
--- app/components/Products/ProductsList.tsx
+++ app/components/Products/ProductsList.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React, { useEffect, useState } from 'react'
+import { DotLoader, PacmanLoader } from 'react-spinners';
 
 
 const ProductsList = () => {
 
-    const [posts, setposts] = useState([]);
+    const [products, setproducts] = useState([]);
     const [loading, setloading] = useState(true);
 
 
@@ -13,9 +14,10 @@ const ProductsList = () => {
             try {
                 setloading(true);
                 const response = await fetch("https://fakestoreapi.com/products");
-                const data = await response.json();
+                let data = await response.json();
+
                 console.log(data);
-                setposts(data);
+                setproducts(data.splice(0, 12));
                 setloading(false);
 
             } catch (error) {
@@ -31,7 +33,35 @@ const ProductsList = () => {
 
 
     return (
-        <div>ProductsList</div>
+        <div>
+
+            {loading ?
+                <div className='w-full h-[90vh] flex justify-center items-center'>
+                    <DotLoader color='#fe019a' />
+                </div>
+                : ""}
+
+            {products && products.length > 0 ?
+
+                <div className='w-full flex flex-wrap '>
+                    {products.map((product) => {
+                        return (
+                            <div className='h-[30vh] w-[100%] md:w-[50%] lg:w-[25%] flex justify-center items-center border-[1.3px] border-palette-lines'>
+                                hello
+                            </div>
+                        )
+                    })}
+                </div>
+
+                :
+
+                <div className='w-full h-[90vh] flex justify-center items-center p-4 text-center text-palette-text text-2xl font-text'>
+                    If you're not seeing anything here, we're either bankrupt, or we're having a party because we sold everything out.
+                </div>
+
+            }
+
+        </div>
     )
 }
 
